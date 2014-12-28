f43f13a5371c9c8a2f7ab90307f998f941cf4263
dempsey1983 <dempsey1983@163.com>
2014-12-28 13:16:09 +0800

    散标投资：增加数据库查询输出；
    Signed-off-by: dempsey1983 <dempsey1983@163.com>

--- inverstment.php
+++ inverstment.php
@@ -4,9 +4,163 @@
 	}
 	class inverstPage extends Page 
 	{
-		public $content = "<p>欢迎光临理财树</p>";
+		public function DisplayStytles() {
+		?>
+		<link rel="stylesheet" type="text/css" href="./css/reset.css"/>
+		<link rel="stylesheet" type="text/css" href="./css/info.css"/>
+		<link rel="stylesheet" type="text/css" href="./css/sanbiao.css" />
+		<?PHP
+		}
 		public function DisplayMain() {
-			echo $this->content;
+			include_once("./sql/connect.php");
+			$query = mysql_query("select * from tb_borrow");
+			$num_result = mysql_num_rows($query);
+//			echo "<p>Number of rows found: ".$num_result."</p>";
+/*		
+			for($i = 0; $i < $num_result; $i++) {
+				$row = mysql_fetch_array($query);
+				echo "<p><strong>".($i+1).". Title: ";
+				echo htmlspecialchars(stripslashes($row['amount']));
+				echo "</strong></p>";
+			}
+*/
+		?>
+		<div id="main">
+        	<div id="main_top">
+            	<div id="main_top1">
+                 <h1>我要理财</h1>
+        		<img src="images/jiantou.png" alt="" /> 
+        		<h2>散标投资列表</h2>
+             </div>
+             </div> 
+              <div id="main_middle">
+                	<div id="main_middle_left">
+                    	<div id="main_middle_left_top">
+                        	<div id="main_middle_left_top1">筛选投资项目</div>
+                            <div id="main_middle_left_top2">
+                            <a>多选</a>
+                            </div>
+                        </div>
+                        <table id="main_middle_bottom">
+  							<tr>
+   							  <td> 标的类型</td>
+                              <td> <input class="ax" type="button" value="不限"></td>
+    						  <td> <input class="ax" type="button" value="实地认证标"></td>
+                              <td> <input class="ax" type="button" value="信用认证标"></td>
+                              <td> <input class="ax" type="button" value="机构担保标"></td>
+                              <td> <input class="ax" type="button" value="智能理财标"></td>
+  							</tr>
+  							<tr>
+    						  <td> 借款期限</td>
+                              <td> <input class="ax" type="button" value="不限"></td>
+    						  <td> <input class="ax" type="button" value="6个月及以下"></td>
+                              <td> <input class="ax" type="button" value="7-12个月"></td>
+                              <td> <input class="ax" type="button" value="13-24个月"></td>
+                              <td> <input class="ax" type="button" value="25个月及以上"></td>
+ 							</tr>
+ 							<tr>
+   							 <td> 认证等级</td>
+                              <td> <input class="ax" type="button" value="不限"></td>
+                              <td> <input class="ax" type="button" value="A"></td>
+                              <td> <input class="ax" type="button" value="B"></td>
+                              <td> <input class="ax" type="button" value="C"></td>
+                              <td> <input class="ax" type="button" value="D"></td>
+  							</tr>
+						</table>                    
+                </div>
+              </div>
+             <div id="main_bottom">
+             	<div id="main_bottom_top">
+                	<div id="main_bottom_top1">投资列表</div>
+                    <div id="main_bottom_top2"><a  href="#" target="_blank">理财计算器</a></div>
+                    <div class="main_bottom_top3">
+                    	<div class="main_bottom_top3_1">累计成交总金额</div>
+                        <div class="main_bottom_top3_2">亿元</div>
+                    </div>
+                    <div class="main_bottom_top3">
+                    	<div class="main_bottom_top3_1">累计成交总金额</div>
+                        <div class="main_bottom_top3_2">亿元</div>
+                    </div>
+                    <div class="main_bottom_top3">
+                    	<div class="main_bottom_top3_1">累计成交总金额</div>
+                        <div class="main_bottom_top3_2">亿元</div>
+                    </div>
+                 </div>	
+                 <table>
+  					<tr bgcolor = "#ccccFF">
+  						<td width="40"></td>
+    					<td width="140">借款标题</td>
+    					<td width="180">信用等级</td>
+                        <td width="180">年利率</td>
+                        <td width="180">金额</td>
+                        <td width="180">期限</td>
+                        <td width="180">进度</td>                       
+  					</tr>
+  					<?php 
+  						//echo "<p>Number of rows found: ".$num_result."</p>";
+						for($i = 0; $i < $num_result; $i++) {
+							$row = mysql_fetch_array($query);
+							
+							?>
+							<tr>
+							<td width="40"><?php 
+								
+								switch($row['type']) {
+									case 1:
+										echo '信';
+										break;
+									default:
+										echo '实';
+										break;
+								}
+								
+								?></td>
+							<td width="140"><?php 
+								echo '<a href="detailPage.php?loanId='.$row['id'].'">';
+								echo $row['title'];
+								echo '</a>';
+								?></td>
+							<td width="180"><?php 
+								$str = "select credit_rating from tb_users where id=".$row['id'];
+								//echo $str;
+								$credit_rating_q = mysql_query($str);
+								$credit_rating_r = mysql_fetch_array($credit_rating_q);
+								$credit_rating = $credit_rating_r['credit_rating'];
+								//echo $credit_rating;
+								switch ($credit_rating){
+									case 1:
+										echo 'HR';break;
+									case 2:
+										echo 'E';break;
+									case 3:
+										echo 'D';break;
+									case 4:
+										echo 'C';break;
+									case 5:
+										echo 'B';break;
+									case 6:
+										echo 'A';break;
+									case 7:
+										echo 'AA';break;
+									case 8:
+										echo 'AAA';break;
+									default:
+										echo 'HR';break;
+								}
+								
+							?></td>
+							<td width="180"><?php echo $row['rate'];?></td>
+							<td width="180"><?php echo $row['amount'];?></td>
+							<td width="180"><?php echo $row['term']."个月";?></td>
+							<td width="180"><?php echo $row['schedule'];?></td>
+							</tr>
+							<?php
+						}
+  					?>
+				</table> 
+             </div> 
+      	</div>
+		<?php
 		}
 	}
 	$subpage = new inverstPage();
--- sql/create_register.sql
+++ sql/create_register.sql
@@ -6,7 +6,7 @@ to bookmgr;
 
 create table tb_registers
 (
-	id    	int	unsigned 	not null 	auto_increment primary key,
+	id    			int	unsigned 	not null 	auto_increment primary key,
 	username		char(50) 		not null,
 	email 			char(100)		not null,
 	password 		char(32)		not null,
@@ -16,12 +16,13 @@ create table tb_registers
 	status				int				not null
 );
 
-create table users
+create table tb_users
 (
-	id			int	unsigned 	not null 	references registers(id),
-	name				char(50)			not null,
-	IDnumber		char(18)			not null,
-	cellPhoneNumber	char(11)	not null,
+	id					int	unsigned 		not null 	references registers(id),
+	name				char(50)				not null,
+	credit_rating	int unsigned 	not null,
+	IDnumber		char(18)				not null,
+	cellPhoneNumber	char(11)		not null,
 	gender			char(1),
 	birthday		char(8),
 	education 	char(1),
@@ -31,6 +32,70 @@ create table users
 	business		char(100),
 	scaleOfCompany  int unsigned,
 	position		char(50),
-	monthlyPorfit	int unsigned,
-)type=InnoDB;
+	monthlyPorfit	int unsigned	
+);
+insert into tb_users (id, name, credit_rating) values
+('1', '赵大', '1'),
+('2', '钱二', '1'),
+('3', '孙三', '1'),
+('4', '李四', '1'),
+('5', '周五', '1'),
+('6', '吴六', '1'),
+('7', '郑七', '1'),
+('8', '王八', '1');
+insert into tb_users (id, name, credit_rating) values
+('9',  '赵1', '1'),
+('10', '钱2', '2'),
+('11', '孙3', '3'),
+('12', '李4', '4'),
+('13', '周5', '5'),
+('14', '吴6', '6'),
+('15', '郑7', '7'),
+('16', '王8', '8');
+/*schedule: 
+0, 申请
+1, 批准
+2，投标
+3，满标
+4，过账，还款中
+5、还款完成*/
+/*type:
+1、信用认证
+2、实地认证
+3、机构担保
+4、智能理财*/
+create table tb_borrow
+(
+	id 			int 				unsigned not null references registers(id),
+	type		int					not null,
+	title 	text				not null,
+	rate		double			not null,
+	amount	int unsigned not null,
+	term		int	unsigned not null,
+	schedule	int unsigned not null,
+	amount_in int unsigned not null,
+	amount_out int unsigned not null
+);
+
+
+
+insert into tb_borrow values
+('1', '2', '资金周转', 	'0.10', 	'78500', '6', '2', '28500', '0'),
+('2', '1', '装修', 			'0.08', 	'32700', '7', '2', '12700', '0'),
+('3', '3', '扩大生产', 	'0.132', '20000', '5', '2', '15000', '0'),
+('4', '1', '买车', 			'0.114', '30000', '8', '3', '30000', '0'),
+('5', '1', '买房', 			'0.126', '40500', '12', '3', '40500', '0'),
+('6', '2', '扩大经营', 	'0.126', '77800', '13', '3', '77800', '0'),
+('7', '2', '资金周转', 	'0.114', '55400', '15', '4', '55400', '0'),
+('8', '1', '旅游', 			'0.125', '53600', '20', '5', '53600', '0'),
+('9', '1', '日常生活消费', '0.110', '54400', '24', '5', '54400', '0'),
+('10', '1', '婚礼筹备', 	'0.105', '67000', '25', '4', '67000', '0'),
+('11', '1', '教育支出', 	'0.1', '107200', '26', '3', '107200', '0'),
+('12', '3', '做生意', 		'0.1', '27800', '9', '3', '27800', '0'),
+('13', '3', '创业', 			'0.1', '68000', '15', '4', '68000', '0'),
+('14', '1', '年底备货', 	'0.1', '20000', '18', '2', '20000', '0'),
+('15', '3', '扩大业务', 	'0.1', '50000', '30', '3', '50000', '0'),
+('16', '2', '资金周转', 	'0.1', '100000', '36', '4', '100000', '0');
+
+
 
